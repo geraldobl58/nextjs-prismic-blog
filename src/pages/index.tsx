472f9a0ab6e187bad27c08895318a9705a76b0dd
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-04 15:34:40 -0300

    :art: Creating global css styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/home.module.scss';
 export default function Home() {
   return (
     <div>
-      <h1 className={styles.title}>
+      <h1>
         Olá <span>Dev!</span>
       </h1>
     </div>
