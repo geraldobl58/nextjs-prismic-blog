0614e1ec5b32fafe023fc27d910dc616ca04f932
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-05 13:51:19 -0300

    :sparkles: Incremental Static Generation

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,12 +1,48 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
 
-export default function Post() {
+type Comment = {
+  id: string;
+  body: string;
+};
+
+type CommentsProps = {
+  comments: Comment[];
+};
+
+export default function Post({ comments }: CommentsProps) {
   const router = useRouter();
+
+  if (router.isFallback) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div>
-      <h1>Post: {router.query.id}</h1>
-      <p>{router.asPath}</p>
+      <ul>
+        {comments.map(comment => (
+          <li key={comment.id}>{comment.body}</li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<CommentsProps> = async context => {
+  const { id } = context.params;
+
+  const response = await fetch(`http://localhost:3333/comments?postId=${id}`);
+  const comments = await response.json();
+
+  return {
+    props: { comments },
+    revalidate: 5,
+  };
+};
