6b664379ac802bb3c86176d44af69431101dd44a
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-05 09:50:41 -0300

    :sparkles: Client Side Rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
+import { useState, useEffect } from 'react';
+
+type Post = {
+  id: string;
+  title: string;
+};
+
 export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/posts').then(response => {
+      response.json().then(data => {
+        setPosts(data);
+      });
+    });
+  }, []);
+
   return (
     <div>
-      <h1>
-        Olá <span>Dev!</span>
-      </h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
