01f5d94e59a2954ba829c45059d1a0b9fbfeac95
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-05 16:05:04 -0300

    :art: Creating content for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,25 @@
-import { GetServerSideProps } from 'next';
-
 import SEO from '../components/SEO';
 
-type Post = {
-  id: string;
-  title: string;
-};
+import styles from '../styles/home.module.scss';
 
-type HomeProps = {
-  posts: Post[];
-};
-
-export default function Home({ posts }: HomeProps) {
+export default function Home() {
   return (
     <div>
       <SEO title="Inicio" />
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
+
+      <main className={styles.content}>
+        <section className={styles.section}>
+          <span>Hello Dev</span>
+          <h1>
+            Bem vindo e bem vinda ao <br /> <span>Nextjs</span> Blog
+          </h1>
+          <p>
+            Um blog desenvolvido com
+            <span> NextJS e Prismic CMS</span>
+          </p>
+        </section>
+        <img src="/home.svg" alt="Nextjs Blog" />
+      </main>
     </div>
   );
 }
-
-export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
-  const response = await fetch('http://localhost:3333/posts');
-  const posts = await response.json();
-
-  return {
-    props: {
-      posts,
-    },
-  };
-};
