6c8a508e9dfedd4d2877ab271e9588e2b8a50983
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-05 17:10:14 -0300

    :art: Structuring the post listing page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,36 +1,36 @@
+import Link from 'next/link';
 import { GetStaticProps } from 'next';
 
 import SEO from '../../components/SEO';
 
-type Post = {
-  id: string;
-  title: string;
-};
+import styles from './posts.module.scss';
 
-type PostsProps = {
-  posts: Post[];
-};
-
-export default function Posts({ posts }: PostsProps) {
+export default function Posts() {
   return (
-    <div>
+    <>
       <SEO title="Posts" />
-      <h1>Posts</h1>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    </div>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <Link href="#">
+            <a>
+              <time>05 de Agosto de 2021</time>
+              <strong>Titulo</strong>
+              <p>Lorem ipsum dolor sit am</p>
+            </a>
+          </Link>
+        </div>
+      </main>
+    </>
   );
 }
 
-export const getStaticProps: GetStaticProps<PostsProps> = async () => {
-  const response = await fetch('http://localhost:3333/posts');
-  const posts = await response.json();
+// export const getStaticProps: GetStaticProps = async () => {
+//   const response = await fetch('http://localhost:3333/posts');
+//   const posts = await response.json();
 
-  return {
-    props: { posts },
-    revalidate: 5,
-  };
-};
+//   return {
+//     props: {},
+//     revalidate: 60 * 60 * 12, // horas
+//   };
+// };
