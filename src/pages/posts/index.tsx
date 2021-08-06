4084d512560f056410d3937d3cd6faf6ce005877
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-06 08:07:55 -0300

    :lock: Adicionando arquivo .env

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,6 +1,11 @@
 import Link from 'next/link';
 import { GetStaticProps } from 'next';
 
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
+import { getPrismicClient } from '../../services/prismic';
+
 import SEO from '../../components/SEO';
 
 import styles from './posts.module.scss';
@@ -25,12 +30,25 @@ export default function Posts() {
   );
 }
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const response = await fetch('http://localhost:3333/posts');
-//   const posts = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-//   return {
-//     props: {},
-//     revalidate: 60 * 60 * 12, // horas
-//   };
-// };
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.content'],
+    },
+  );
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+    };
+  });
+
+  return {
+    props: {},
+    revalidate: 60 * 60 * 12, // horas
+  };
+};
